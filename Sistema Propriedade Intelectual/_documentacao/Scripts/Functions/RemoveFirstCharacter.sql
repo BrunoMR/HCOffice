0c32a096acc0126ff0154f16c9b198469986e7cc
brunomachadoDbc <bruno.machado@dbccompany.com.br>
2019-09-17 19:42:37 -0300

    Adjust

--- Sistema Propriedade Intelectual/_documentacao/Scripts/Functions/RemoveFirstCharacter.sql
+++ Sistema Propriedade Intelectual/_documentacao/Scripts/Functions/RemoveFirstCharacter.sql
@@ -1,10 +1,3 @@
--- =============================================
--- Author:		Bruno Machado Rodrigues
--- Create date: 20/10/2017
--- Description:	Esta função irá retirar o primeiro caracter do termo enviado por parâmetro
-CREATE FUNCTION [dbo].[RemoveFirstCharacter]
-(
-  @word VARCHAR(2000)-- =============================================
 -- Author:		Bruno Machado Rodrigues
 -- Create date: 20/10/2017
 -- Description:	Esta função irá retirar o primeiro caracter do termo enviado por parâmetro
@@ -27,22 +20,3 @@ BEGIN
   RETURN @word
 END
 go
-
-
-)
-RETURNS VARCHAR(2000)
-AS
-BEGIN
-
-  IF (LEN(@word) <= 0)
-    RETURN NULL;
-
-  IF (LEFT(@word, 1) = '''')
-  BEGIN
-    RETURN SUBSTRING(@word, 2, LEN(@word) - 1)
-  END
-
-  RETURN @word
-END
-go
-
