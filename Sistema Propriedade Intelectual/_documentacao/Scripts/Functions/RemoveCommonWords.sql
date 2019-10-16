04321a51ae36305c6868233c6fda9f7b3f625e6b
BrunoMR <brunomachadorodrigues@gmail.com>
2019-10-16 19:35:04 -0300

    the final adjusts for remove the commons words of the term

--- Sistema Propriedade Intelectual/_documentacao/Scripts/Functions/RemoveCommonWords.sql
+++ Sistema Propriedade Intelectual/_documentacao/Scripts/Functions/RemoveCommonWords.sql
@@ -15,11 +15,11 @@ AS
 BEGIN
 
     DECLARE
-        @words AS TABLE (word VARCHAR(255))
+        @words AS TABLE (Id INT IDENTITY(1,1), Word VARCHAR(255))
 
     INSERT INTO @words
     select
-      ltrim(rtrim(string_agg(splitdata, ' ')))
+      ltrim(rtrim(splitdata))
     from
         dbo.SplitString(@word, ' ')  spl
     where
@@ -37,78 +37,7 @@ BEGIN
             SELECT
                 distinct
                 STUFF((SELECT
-                            DISTINCT
-                                ', ' + wrd2.word
-                       FROM
-                            @words wrd2
-                      FOR XML PATH('')), 1, 2, '')
-            FROM
-                 @words wrd
-            GROUP BY
-                wrd.word
-        )
-
-  RETURN @word
-END
-go
-
-
-DECLARE
-        @words AS TABLE (word VARCHAR(255))
-
-declare
-    @word   VARCHAR(255) = 'bruno opa machado teste cabelo no sofa as vezes EITa opa'
-
-    select
---        ltrim(rtrim(string_agg(splitdata, ' ')))
-        splitdata
-    from
-        dbo.SplitString(@word, ' ')  spl
-    where
-        not exists(  select
-                        1
-                    from
-                        PALAVRA_USO_COMUM
-                    where
-                        Palavra = spl.splitdata
-                )
-
-
-
-
-
-declare
-    @word   VARCHAR(255) = 'bruno opa machado teste cabelo no sofa as vezes EITa opa MAIS UMA vez cabelo'
-
-
-DECLARE
-        @words AS TABLE (Id INT IDENTITY(1,1), Word VARCHAR(255))
-
-    INSERT INTO @words
-    select
-      ltrim(rtrim(splitdata))
-    from
-        dbo.SplitString(@word, ' ')  spl
-    where
-        not exists(  select
-                        1
-                    from
-                        PALAVRA_USO_COMUM
-                    where
---                         Numero_Classe =  @class
---                         AND
-                          Palavra = spl.splitdata
-                )
-
-    select * from @words
-
-    SET @word =
-        (
-            SELECT
-                distinct
-                STUFF((SELECT
-                            --DISTINCT
-                                ' ' + wrd2.Word
+                            ' ' + wrd2.Word
                        FROM
                             @words wrd2
                        ORDER BY
@@ -118,30 +47,8 @@ DECLARE
                  @words wrd
             GROUP BY
                 wrd.Word
---             ORDER BY
---                 wrd.Id
         )
 
-select @word [@word]
-
-
-
-
-
-
-
-
-
-
-SELECT
-    distinct
-    STUFF((SELECT
-                DISTINCT
-                    ', ' + spl2.splitdata
-           FROM
-                dbo.SplitString(@word, ' ')  spl2
-          FOR XML PATH('')), 1, 2, '')
-FROM
-     dbo.SplitString(@word, ' ')  spl
-GROUP BY
-    spl.splitdata
+  RETURN @word
+END
+go
