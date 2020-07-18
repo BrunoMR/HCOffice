07aa5797abe7cc8d4cb02bda6c60e4375c4028f7
BrunoMR <brunomachadorodrigues@gmail.com>
2020-07-18 17:51:42 -0300

    modify objects to syncronize natures from server to local

--- Sistema Propriedade Intelectual/_documentacao/Scripts/Types/PROCESSOTYPE.sql
+++ Sistema Propriedade Intelectual/_documentacao/Scripts/Types/PROCESSOTYPE.sql
@@ -8,37 +8,40 @@ IF NOT EXISTS(
 			 )
 BEGIN
 
-	CREATE TYPE PROCESSOTYPE AS TABLE
+	create type PROCESSOTYPE as table
 	(
-		NUMERO                varchar(20) not null,
-		NOME_TITULAR          varchar(500),
-		CPF_CNPJ_INPI_TITULAR varchar(100),
-		PAIS_TITULAR          varchar(50),
-		UF_TITULAR            char(2),
-		NOME_PROCURADOR       varchar(500),
-		MARCA                 varchar(1000),
-		MARCA_ORTOGRAFADA     varchar(1000),
-		MARCA_NAO_ORTOGRAFADA varchar(1000),
-		PRIORIDADE            varchar(100),
-		DATA_PRIORIDADE       date,
-		NOME_PAIS_PRIORIDADE  char(2),
-		TIPO_APRESENTACAO     int,
-		TIPO_NATUREZA         int,
-		CLASSE_INTERNACIONAL  char(5),
-		CLASSE_1              char(5),
-		CLASSE_2              char(5),
-		CLASSE_3              char(5),
-		ESPECIFICACAO         varchar(max),
-		APOSTILA              varchar(max),
-		NUMERO_REFERENCIA     varchar(255),
-		DATA_DEPOSITO         date,
-		DATA_CONCESSAO        date,
-		DATA_REGISTRO         date,
-		DATA_VIGENCIA         date,
-		MARCA_SEM_VOGAIS      varchar(1000),
+		NUMERO                         varchar(20) not null,
+		NOME_TITULAR                   varchar(500),
+		CPF_CNPJ_INPI_TITULAR          varchar(100),
+		PAIS_TITULAR                   varchar(50),
+		UF_TITULAR                     char(2),
+		NOME_PROCURADOR                varchar(500),
+		MARCA                          varchar(1000),
+		MARCA_ORTOGRAFADA              varchar(1000),
+		MARCA_NAO_ORTOGRAFADA          varchar(1000),
+		PRIORIDADE                     varchar(100),
+		DATA_PRIORIDADE                date,
+		NOME_PAIS_PRIORIDADE           char(2),
+		TIPO_APRESENTACAO              int,
+		TIPO_NATUREZA                  int,
+		CLASSE_INTERNACIONAL           char(5),
+		CLASSE_1                       char(5),
+		CLASSE_2                       char(5),
+		CLASSE_3                       char(5),
+		ESPECIFICACAO                  varchar(max),
+		APOSTILA                       varchar(max),
+		NUMERO_REFERENCIA              varchar(255),
+		DATA_DEPOSITO                  date,
+		DATA_CONCESSAO                 date,
+		DATA_REGISTRO                  date,
+		DATA_VIGENCIA                  date,
+		MARCA_SEM_VOGAIS               varchar(1000),
 		NUMERO_INSCRICAO_INTERNACIONAL varchar(20),
-		DATA_RECEBIMENTO_INPI date
+		DATA_RECEBIMENTO_INPI          date,
+		TIPO_NATUREZA_DESCRICAO        varchar(100)
+		primary key nonclustered (NUMERO)
 	)
+	go
     
 PRINT 'O Type "PROCESSOTYPE" foi criado com sucesso'
 END
