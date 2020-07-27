7c7fa9ddde7a964606b185694ba1f3b72d72134f
BrunoMR <brunomachadorodrigues@gmail.com>
2020-07-27 00:05:59 -0300

    tables modified to prefix,sufix and radicals be able to work

--- Sistema Propriedade Intelectual/_documentacao/Scripts/Tables/PROCESS_TO_COLLIDE.sql
+++ Sistema Propriedade Intelectual/_documentacao/Scripts/Tables/PROCESS_TO_COLLIDE.sql
@@ -1,14 +1,30 @@
-create table PROCESS_TO_COLLIDE
-(
-    ID_PROCESSO       int          not null,
-    NUMERO            varchar(20)  not null,
-    MARCA             varchar(1000),
-    MARCA_ORTOGRAFADA varchar(1000),
-    MARCA_SEM_VOGAIS  varchar(8000),
-    NOME_TITULAR      varchar(500),
-    DATA_DEPOSITO     varchar(30),
-    DATA_CONCESSAO    varchar(30),
-    CODIGO            varchar(100) not null,
-    NOME_PROCURADOR   varchar(500)
-)
-go
+IF NOT EXISTS (
+				SELECT
+				 *
+				FROM 
+				  SYSOBJECTS
+				WHERE 
+				  XTYPE = 'U'
+				  AND NAME = 'PROCESS_TO_COLLIDE' 
+			 )
+BEGIN
+	create table PROCESS_TO_COLLIDE
+	(
+		ID_PROCESSO       int          not null,
+		NUMERO            varchar(20)  not null,
+		MARCA             varchar(1000),
+		MARCA_ORTOGRAFADA varchar(1000),
+		MARCA_SEM_VOGAIS  varchar(8000),
+		NOME_TITULAR      varchar(500),
+		DATA_DEPOSITO     varchar(30),
+		DATA_CONCESSAO    varchar(30),
+		CODIGO            varchar(100) not null,
+		NOME_PROCURADOR   varchar(500),
+		MarcaModificada   varchar(1000)
+	)
+	
+	PRINT 'A Tabela "PROCESS_TO_COLLIDE" foi criada com sucesso'
+END
+ELSE
+  PRINT 'A Tabela "PROCESS_TO_COLLIDE" já existe!'
+GO
